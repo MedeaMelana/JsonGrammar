0965ebc31c6479349d0f1bbb2b2b7a41431cbf56
Martijn van Steenbergen <martijn@van.steenbergen.nl>
2011-05-01 17:12:52 +0200

    Use Strings for property names Added Maybe instance

--- JsonGrammar.hs
+++ JsonGrammar.hs
@@ -6,18 +6,18 @@ module JsonGrammar where
 
 import Iso
 
-import Prelude hiding (id, (.), head)
+import Prelude hiding (id, (.), head, maybe)
 
 import Control.Applicative
 import Control.Category
+import Control.Monad
 
 import Data.Aeson
 import Data.Aeson.Types
 import qualified Data.Map as M
+import Data.String
 import Data.Text (Text)
 import qualified Data.Vector as V
-
-import Control.Monad
 
 
 -- Json type class and instances
@@ -43,6 +43,11 @@ instance Json Int where
 instance Json [Char] where
   grammar = liftAeson
 
+instance Json a => Json (Maybe a) where
+  grammar = maybe grammar
+
+-- instance Json 
+
 -- | Convert any Aeson-enabled type to an isomorphism.
 liftAeson :: (FromJSON a, ToJSON a) => Iso Value a
 liftAeson = Iso from to
@@ -57,38 +62,46 @@ forceToJson context value =
     Nothing     -> error (context ++
       ": could not convert Haskell value to JSON value")
 
+fromJson :: Json a => Value -> Maybe a
+fromJson = convert grammar
+
+toJson :: Json a => a -> Maybe Value
+toJson = convert (inverse grammar)
+
 
 -- Object grammars
 
-prop :: Json a => Text -> Iso (Object :- t) (Object :- a :- t)
+prop :: Json a => String -> Iso (Object :- t) (Object :- a :- t)
 prop = propBy grammar
 
-propBy :: Iso Value a -> Text -> Iso (Object :- t) (Object :- a :- t)
+propBy :: Iso Value a -> String -> Iso (Object :- t) (Object :- a :- t)
 propBy g name = duck (stack g) . rawProp name
 
-rawProp :: Text -> Iso (Object :- t) (Object :- Value :- t)
+rawProp :: String -> Iso (Object :- t) (Object :- Value :- t)
 rawProp name = Iso from to
   where
+    textName = fromString name
     from (o :- r) = do
-      value <- M.lookup name o
-      return (M.delete name o :- value :- r)
+      value <- M.lookup textName o
+      return (M.delete textName o :- value :- r)
     to (o :- value :- r) = do
-      guard (M.notMember name o)
-      return (M.insert name value o :- r)
+      guard (M.notMember textName o)
+      return (M.insert textName value o :- r)
 
-fixedProp :: Json a => Text -> a -> Iso (Object :- t) (Object :- t)
+fixedProp :: Json a => String -> a -> Iso (Object :- t) (Object :- t)
 fixedProp name value = rawFixedProp name (forceToJson "fixedProp" value)
 
-rawFixedProp :: Text -> Value -> Iso (Object :- t) (Object :- t)
+rawFixedProp :: String -> Value -> Iso (Object :- t) (Object :- t)
 rawFixedProp name value = stack (Iso from to)
   where
+    textName = fromString name
     from o = do
-      value' <- M.lookup name o
+      value' <- M.lookup textName o
       guard (value' == value)
-      return (M.delete name o)
+      return (M.delete textName o)
     to o = do
-      guard (M.notMember name o)
-      return (M.insert name value o)
+      guard (M.notMember textName o)
+      return (M.insert textName value o)
 
 object :: Iso (Object :- t1) (Object :- t2) -> Iso (Value :- t1) t2
 object (Iso from to) = Iso from' to'
@@ -103,7 +116,7 @@ object (Iso from to) = Iso from' to'
       return (Object o :- t1)
 
 
--- Arrays
+-- Miscellaneous
 
 array :: Iso Value a -> Iso Value [a]
 array (Iso from to) = Iso from' to'
@@ -113,8 +126,13 @@ array (Iso from to) = Iso from' to'
       mapM from (V.toList vector)
     to' xs = Array . V.fromList <$> mapM to xs
 
-
--- Literals
+maybe :: Iso Value a -> Iso Value (Maybe a)
+maybe (Iso from to) = Iso from' to'
+  where
+    from' Null   = return Nothing
+    from' v      = Just <$> from v
+    to' Nothing  = return Null
+    to' (Just v) = to v
 
 lit :: Json a => a -> Iso (Value :- t) t
 lit = rawLit . forceToJson "lit"
