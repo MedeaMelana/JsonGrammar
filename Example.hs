bdc07fbb1d814db5238b636c60c9077fe2759e69
Martijn van Steenbergen <martijn@van.steenbergen.nl>
2011-05-03 21:49:22 +0200

    Added type signatures.

--- Example.hs
+++ Example.hs
@@ -20,11 +20,17 @@ data Attachment = Attachment
   , atThumbnail   :: Maybe String
   } deriving Show
 
+attachment :: Iso (Maybe String :- String :- AttachmentType :-
+  String :- Maybe String :- t) (Attachment :- t)
 attachment = $(deriveIsos ''Attachment)
+
 
 data AttachmentType = AttVideo | AttImage | AttHtml
   deriving Show
 
+attVideo :: Iso a (AttachmentType :- a)
+attImage :: Iso a (AttachmentType :- a)
+attHtml  :: Iso a (AttachmentType :- a)
 (attVideo, attImage, attHtml) = $(deriveIsos ''AttachmentType)
 
 
