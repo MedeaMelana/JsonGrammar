9ba815434ade7c0ba472b1290700b1fdf1ffba38
Martijn van Steenbergen <martijn@van.steenbergen.nl>
2011-05-08 12:35:46 +0200

    Added module Data.Iso

---
+++ Data/Iso.hs
@@ -0,0 +1,12 @@
+-- | Convenience module that re-exports the available submodules.
+module Data.Iso (
+
+  module Data.Iso.Core,
+  module Data.Iso.Common,
+  module Data.Iso.TH
+
+  ) where
+
+import Data.Iso.Core
+import Data.Iso.Common
+import Data.Iso.TH
